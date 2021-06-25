ad3fb96ade127378429bb074dc07ddfc77e70a3e
= <hessedoneen@gmail.com>
2021-06-25 14:57:22 -0400

    copied unit jest into node-runner for edits

---
+++ packages/@vue/cli-plugin-node-runner/presets/default/jest-preset.js
@@ -0,0 +1,42 @@
+let vueJest = null
+try {
+  vueJest = require.resolve('vue-jest')
+} catch (e) {
+  throw new Error('Cannot resolve "vue-jest" module. Please make sure you have installed "vue-jest" as a dev dependency.')
+}
+
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    // tell Jest to handle *.vue files
+    'vue'
+  ],
+  transform: {
+    // process *.vue files with vue-jest
+    '^.+\\.vue$': vueJest,
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
+    require.resolve('jest-transform-stub'),
+    '^.+\\.jsx?$': require.resolve('babel-jest')
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  // support the same @ -> src alias mapping in source code
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  // serializer for snapshots
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.[jt]s?(x)',
+    '**/__tests__/*.[jt]s?(x)'
+  ],
+  // https://github.com/facebook/jest/issues/6766
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname')
+  ]
+}
